0db0390f5da23de231960b4a5a4fb929396b5731
곽세진_2004_anl <Reblackraven@Gmail.com>
2021-11-20 00:23:43 +0900

    implement number of comma & Mapping ac btn

--- project/calculate/calculate.js
+++ project/calculate/calculate.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class Calculate {
   constructor() {}
 }
@@ -21,28 +23,8 @@ class Mapping {
 
 const value = document.getElementById("value");
 const buttons = document.querySelectorAll("button");
+const acBtn = document.querySelector(".ac");
 
-/* observer 적용해보기
-const observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    console.log(mutation, mutation.type);
-  });
-});
-
-const config = {
-  attributes: true,
-  childList: true,
-  characterData: true,
-  subtree: true || null,
-  attributeOldValue: true || null,
-  characterDataOldValue: true || null,
-};
-
-observer.observe(value, config);
-
-observer.disconnect();
-*/
-// 1 & 2 (나중에 2번 분리하기)
 let numberOfStack = [];
 
 const removeCommaOfValue = () => {
@@ -55,42 +37,53 @@ const removeCommaOfValue = () => {
 
 // 3
 const insertCommaCallbackFunction = () => {
-  if (parseInt(numberOfStack.length / 3) === 1) {
-    // 3, 4, 5자리
-    numberOfStack.length > 3 &&
-      numberOfStack.splice(numberOfStack.length % 3, 0, ",");
-  } else {
-    // 콤마 손보기
-    // 6자리 수 일 때 ex) 123,456
-    if (numberOfStack.length % 3 === 0) {
-      numberOfStack.splice(3, 0, ",");
-    } else {
-      // 7, 8 자리
-      if (numberOfStack.length === 7) {
-        numberOfStack.splice(numberOfStack.length % 3, 0, ",");
-        numberOfStack.splice((numberOfStack.length % 3) + 3, 0, ",");
-      } else {
-        numberOfStack.splice(numberOfStack.length % 3, 0, ",");
-        numberOfStack.splice((numberOfStack.length % 3) + 6, 0, ",");
-      }
-    }
-  }
-  // 1자리 & 9자리 이상
+  // 정규표현식을 사용하면 편리하다.
+  return numberOfStack.join("").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
 const handleNumberButtonClick = (button, insertCommaCallbackFunction) => {
-  // 숫자일 때 체크
+  // 자리수에 붙는 콤마 제거 함수
   removeCommaOfValue();
+
+  // 숫자일 때 연산
   if (Object.values(button.classList).includes("number")) {
     // value 글자 수 체크
     if (value.innerText.length === 1 && numberOfStack.length === 0) {
       // 지금 첫글자 일 때 0이 아니면 숫자 전환 array + / 0이라면 continue
       parseInt(button.innerText) !== 0 && numberOfStack.push(button.innerText);
       value.innerText = button.innerText;
+      parseInt(value.innerText) !== 0 && (acBtn.innerText = "C");
     } else {
+      // 숫자 제한 범위 체크
       numberOfStack.length <= 14 && numberOfStack.push(button.innerText);
-      insertCommaCallbackFunction();
-      value.innerText = numberOfStack.join("");
+      value.innerText = insertCommaCallbackFunction();
+    }
+  } else if (Object.values(button.classList).includes("mapping")) {
+    if (button.classList.contains("ac")) {
+      // 초기화
+      numberOfStack = [];
+      value.innerText = 0;
+      acBtn.innerText = "AC";
+    } else if (button.classList.contains("plusOrMinus")) {
+      // 부호변경
+      numberOfStack[0] === "-"
+        ? numberOfStack.shift()
+        : numberOfStack.unshift("-");
+      value.innerText = insertCommaCallbackFunction();
+    } else if (button.classList.contains("percent")) {
+      // 퍼센트 변경
+      numberOfStack = String(parseInt(numberOfStack.join("")) / 100).split("");
+      value.innerText = insertCommaCallbackFunction();
+    } else if (button.classList.contains("comma")) {
+      if (value.innerText !== "0") {
+        numberOfStack.splice(numberOfStack.length, 0, ".");
+        value.innerText = insertCommaCallbackFunction();
+      } else {
+        numberOfStack.push("0");
+        numberOfStack.push(".");
+
+        value.innerText = insertCommaCallbackFunction();
+      }
     }
   }
 };
--- project/calculate/calculate.md
+++ project/calculate/calculate.md
@@ -5,10 +5,10 @@
 ### Number(숫자)
   - keypad number btn
     - detail(구체화)
-      1. [x] 버튼을 눌렀을 때 button value를 input 창에 표시
-      2. [x] 0일 때는 추가 X
-      3. [ ] 연산이 종료 되는 시점 숫자 크기 체크 후 쉼표 표시
-      4. [x] 숫자가 14자리 이상일 때 숫자 입력 불가(연산만 가능)
+      1. [x] ~~버튼을 눌렀을 때 button value를 input 창에 표시~~
+      2. [x] ~~0일 때는 추가 X~~
+      3. [x] ~~연산이 종료 되는 시점 숫자 크기 체크 후 쉼표 표시~~
+      4. [x] ~~숫자가 14자리 이상일 때 숫자 입력 불가(연산만 가능)~~
       5. [ ] 숫자가 길어질 때 숫자 크기 줄어듬
       6. [ ] 숫자가 14자리보다 커진 연산은 e 단위로 변경 (가장 마지막에 할 것)
 ### Value(값)
@@ -17,13 +17,34 @@
   - +, -, *, /, =
 ### Mapping
   - ac, +/-, %, .
+    - keypad mapping btn
+      - detail(구체화)
+        1. [x] ~~ac button (초기화)~~
+          - [x] ~~AC -> C 글자 변환~~
+        2. [x] ~~+/- plusOrMinus (부호 맵핑)~~
+          - [ ] Operation 배열 따로 만들어 줘서 나중에 맵핑하기
+        3. [ ] % percent (퍼센트 맵핑)
+          - [ ] 맵핑 후 숫자 입력 불가
+          - [ ] round 누르고 퍼센트 누르고 숫자를 누를 때 0 앞에 있음
+        4. [ ] . (round) 처리 (소수점 맵핑)
+          - [ ] +/- 부호 변환시 콤마
+          - [ ] round 자체 숫자 변화시 콤마
+          - [ ] input value가 0일 때 콤마 처리
 
 
 - callback 함수에 인자를 넣을 때 익명함수를 사용
   - 하지만.. 이게 좋은 코드인가??
-    ``` buttons.forEach((element) => {
+    ```javascript
+    buttons.forEach((element) => {
           element.addEventListener("click", () => {
             handleNumberButtonClick(element, addCommaToValue);
           });
         });
     ```
+
+- 함수가 많아짐과 동시에 절차식으로 선언할 때 함수의 의존 관계를 설명하지 못함;;
+
+- 정규 표현식 체크
+  ```javascript
+  numberOfStack.join("").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  ```
