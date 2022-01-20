a1baa2f203f413e022dc79e922f39a62f15e32d3
Blackraven93 <reblackraven@gmail.com>
2022-01-20 15:42:07 +0900

    Chore: 오타 수정

--- oop/design/deepDive/commandPattern.js
+++ oop/design/deepDive/commandPattern.js
@@ -151,7 +151,7 @@
 // console.log(calculator.value)
 
 
-export default class Calculate {
+class Calculate {
     constructor(list) {
       this.contextList = list;
       this.totalContect = [];
@@ -178,7 +178,7 @@ export default class Calculate {
         popedList[i] = newList[i];
       }
   
-      this.contextList = popedList;
+        this.contextList = popedList;
   
       return this.contextList;
     }
@@ -188,53 +188,53 @@ export default class Calculate {
     }
 }
 
-  export default class Operator extends Calculate {
+class Operator extends Calculate {
     constructor(operatorList) {
       super(operatorList);
       this.operator = operatorList[1];
     }
   
-    calculate() {
-      const [num1, num2] = [
-        Number(this.contextList[0]),
-        Number(this.contextList[2]),
-      ];
-      
-      switch (this.operator) {
-        case "+":
-          return this.plus(num1, num2);
-        case "-":
-          if ((num1 - num2) < 0 ) return new Error("뺄셈 결과 음수값 입니다. ");
-          return this.subtract(num1, num2);
-        case "x":
-          return this.multiply(num1, num2);
-        case "÷":
-          if (num2 !== 0) return Math.floor(this.divide(num1, num2));
-          return new Error("0으로 나눌 수 없습니다.")
-        case "=":
-          return this.equal();
-        default:
-          return "Done";
-      }
-    }
-  
     plus(num1, num2) {
-      return num1 + num2;
+        return num1 + num2;
     }
-  
+
     subtract(num1, num2) {
-      return num1 - num2;
+        return num1 - num2;
     }
-  
+
     multiply(num1, num2) {
-      return num1 * num2;
+        return num1 * num2;
     }
-  
+
     divide(num1, num2) {
-      return num1 / num2;
+        return num1 / num2;
     }
-  
+
     equal() {
-      return num1 === undefined || num2 === undefined ? this.calculate() : new Error("정확한 값을 전달해주세요")
+        return num1 === undefined || num2 === undefined ? this.calculate() : new Error("정확한 값을 전달해주세요")
     }
-  }
+
+    calculate() {
+        const [num1, num2] = [
+            Number(this.contextList[0]),
+            Number(this.contextList[2]),
+        ];
+        
+        switch (this.operator) {
+            case "+":
+                return this.plus(num1, num2);
+            case "-":
+                if ((num1 - num2) < 0 ) return new Error("뺄셈 결과 음수값 입니다. ");
+                return this.subtract(num1, num2);
+            case "x":
+                return this.multiply(num1, num2);
+            case "÷":
+                if (num2 !== 0) return Math.floor(this.divide(num1, num2));
+                return new Error("0으로 나눌 수 없습니다.")
+            case "=":
+                return this.equal();
+            default:
+                return "Done";
+        }
+    }
+}
